b067b570f15c3886c9f14e78ff9ed342e5ff8b68
Reem99-sina <reemsina2@gmail.com>
2022-08-05 00:18:19 +0200

    sowell

--- src/component/Productslug.jsx
+++ src/component/Productslug.jsx
@@ -33,13 +33,13 @@ export default function Productslug() {
         loading: true,
         error: ""
     })
-
     useEffect(() => {
         const fetchData = async () => {
             dispatch({ type: "FETCH_REQUER" })
             try {
                 const result = await axios.get(`https://amazon99.herokuapp.com/api/products/slug/${slug}`)
                 dispatch({ type: "FETCH_SUCCESS", payload: result.data })
+                localStorage.setItem("payload", result.data)
             } catch (error) {
                 dispatch({ type: "FETCH_FAILED", payload: error.message })
             }
@@ -61,52 +61,100 @@ export default function Productslug() {
     }
     return (
         loading ? <Spinner animation='border' role='status'><span className='visually-hidden'>loading...</span></Spinner> : error ? <Alert variant='danger'> {error}</Alert> :
-            <div><Row>
-                <Col md={6}>
-                    <img className='img-large' src={product.image} alt={product.name} />
-                </Col>
-                <Col md={3}>
-                    <ListGroup variant='flush'>
-                        <ListGroup.Item>
-                            <h2>{product.name}</h2>
-                            <Helmet>
-                                <title>{product.name}</title>
-                            </Helmet>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            <Rating rating={product.rating} numReviews={product.numReviews}></Rating>
-                        </ListGroup.Item>
-                        <ListGroup.Item>Price:${product.price}</ListGroup.Item>
-                        <ListGroup.Item>Description:<p>{product.description}</p></ListGroup.Item>
-                    </ListGroup>
-                </Col>
-                <Col md={3}>
-
-                    <ListGroup variant='flush'>
-                        <ListGroup.Item>
-                            <Row>
-                                <Col>Price:
-                                </Col>
-                                <Col>${product.price}</Col>
-                            </Row>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            <Row>
-                                <Col>Status:
-                                </Col>
-                                <Col>{product.countInStock > 0 ? <Badge bg='success'>in stock</Badge> : <Badge bg='danger'>not validation</Badge>}</Col>
-                            </Row>
-                        </ListGroup.Item>
-
-                        {product.countInStock > 0 &&
+            localStorage.getItem("payload") ?
+                <div><Row>
+                    <Col md={6}>
+                        <img className='img-large' src={localStorage.getItem("payload").image} alt={localStorage.getItem("payload").name} />
+                    </Col>
+                    <Col md={3}>
+                        <ListGroup variant='flush'>
                             <ListGroup.Item>
-                                <div className='d-grid'>
-                                    <Button onClick={addtoCart} variant='primary'>
-                                        addto chart</Button></div>
-                            </ListGroup.Item>}
+                                <h2>{localStorage.getItem("payload").name}</h2>
+                                <Helmet>
+                                    <title>{product.name}</title>
+                                </Helmet>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Rating rating={localStorage.getItem("payload").rating} numReviews={localStorage.getItem("payload").numReviews}></Rating>
+                            </ListGroup.Item>
+                            <ListGroup.Item>Price:${localStorage.getItem("payload").price}</ListGroup.Item>
+                            <ListGroup.Item>Description:<p>{localStorage.getItem("payload").description}</p></ListGroup.Item>
+                        </ListGroup>
+                    </Col>
+                    <Col md={3}>
 
-                    </ListGroup>
-                </Col>
-            </Row></div>
+                        <ListGroup variant='flush'>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Price:
+                                    </Col>
+                                    <Col>${localStorage.getItem("payload").price}</Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Status:
+                                    </Col>
+                                    <Col>{localStorage.getItem("payload").countInStock > 0 ? <Badge bg='success'>in stock</Badge> : <Badge bg='danger'>not validation</Badge>}</Col>
+                                </Row>
+                            </ListGroup.Item>
+
+                            {localStorage.getItem("payload").countInStock > 0 &&
+                                <ListGroup.Item>
+                                    <div className='d-grid'>
+                                        <Button onClick={addtoCart} variant='primary'>
+                                            addto chart</Button></div>
+                                </ListGroup.Item>}
+
+                        </ListGroup>
+                    </Col>
+                </Row></div>
+                : <div><Row>
+                    <Col md={6}>
+                        <img className='img-large' src={product.image} alt={product.name} />
+                    </Col>
+                    <Col md={3}>
+                        <ListGroup variant='flush'>
+                            <ListGroup.Item>
+                                <h2>{product.name}</h2>
+                                <Helmet>
+                                    <title>{product.name}</title>
+                                </Helmet>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Rating rating={product.rating} numReviews={product.numReviews}></Rating>
+                            </ListGroup.Item>
+                            <ListGroup.Item>Price:${product.price}</ListGroup.Item>
+                            <ListGroup.Item>Description:<p>{product.description}</p></ListGroup.Item>
+                        </ListGroup>
+                    </Col>
+                    <Col md={3}>
+
+                        <ListGroup variant='flush'>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Price:
+                                    </Col>
+                                    <Col>${product.price}</Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Status:
+                                    </Col>
+                                    <Col>{product.countInStock > 0 ? <Badge bg='success'>in stock</Badge> : <Badge bg='danger'>not validation</Badge>}</Col>
+                                </Row>
+                            </ListGroup.Item>
+
+                            {product.countInStock > 0 &&
+                                <ListGroup.Item>
+                                    <div className='d-grid'>
+                                        <Button onClick={addtoCart} variant='primary'>
+                                            addto chart</Button></div>
+                                </ListGroup.Item>}
+
+                        </ListGroup>
+                    </Col>
+                </Row></div>
     )
 }
